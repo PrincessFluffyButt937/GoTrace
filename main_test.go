4343ac80f18cee09bcb50760fe834fceb23212de
PrincessFluffyButt937 <jhosch@seznam.cz>
2026-06-12 20:12:47 +0200

    testing for new parse functions

--- main_test.go
+++ main_test.go
@@ -147,3 +147,103 @@ func TestProjecDataParse(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestTimeParse(t *testing.T) {
+	sample1 := "2024040506332203"
+	exp1 := "time.Date(2024, time.April, 5, 6, 33, 22, 0, time.UTC)"
+
+	parsedTime1, err := parseStringToTime(sample1)
+	if err != nil {
+		println(err.Error())
+		t.Fail()
+	}
+	if parsedTime1.GoString() != exp1 {
+		t.Fail()
+		fmt.Printf("TestTimeParse, parsedTime1 mismatch:\nexp: %s\ngot: %s\n", exp1, parsedTime1)
+	}
+
+	sample2 := "2021050506332403"
+	exp2 := "time.Date(2021, time.May, 5, 6, 33, 24, 0, time.UTC)"
+
+	parsedTime2, err := parseStringToTime(sample2)
+	if err != nil {
+		println(err.Error())
+		t.Fail()
+	}
+	if parsedTime2.GoString() != exp2 {
+		t.Fail()
+		fmt.Printf("TestTimeParse, parsedTime2 mismatch:\nexp: %s\ngot: %s\n", exp2, parsedTime2)
+	}
+
+	sample3 := "2021"
+
+	if _, err := parseStringToTime(sample3); err == nil {
+		t.Fail()
+		fmt.Println("TestTimeParse, sample3: error should have been raised")
+	}
+
+	sample4 := "20Hello506332203"
+
+	if _, err := parseStringToTime(sample4); err == nil {
+		t.Fail()
+		fmt.Println("TestTimeParse, sample4: error should have been raised")
+	}
+}
+
+func TestSortKEyGeneration(t *testing.T) {
+	ref1 := "R1"
+	exp1 := "000001"
+
+	ref2 := "CD146"
+	exp2 := "000146"
+
+	ref3 := "TX123456"
+	exp3 := "123456"
+
+	key1 := refSortKey(ref1)
+	t.Attr(exp1, key1)
+
+	key2 := refSortKey(ref2)
+	t.Attr(exp2, key2)
+
+	key3 := refSortKey(ref3)
+	t.Attr(exp3, key3)
+}
+
+func TestRefToString(t *testing.T) {
+	rList1 := []string{"R10", "R2", "R12", "R3"}
+	exp1 := "R2, R3, R10, R12"
+
+	rList2 := []string{"R3"}
+	exp2 := "R3"
+
+	rList3 := []string{"R1", "R2", "R12", "R3", "R26", "R123456"}
+	exp3 := "R1, R2, R3, R12, R26, R123456"
+
+	rList4 := []string{"R1", "R2", "R12", "R3", "R26", "R123456"}
+	exp4 := "R1, R2, R12, R3, R26, R123456"
+
+	rSring1 := refListToString(rList1, true)
+	if rSring1 != exp1 {
+		t.Fail()
+		fmt.Printf("TestRefToString / exp1 - rString1 mismatch:\nexp: %s\ngot: %s\n", exp1, rSring1)
+	}
+
+	rSring2 := refListToString(rList2, true)
+	if rSring2 != exp2 {
+		t.Fail()
+		fmt.Printf("TestRefToString / exp2 - rString2 mismatch:\nexp: %s\ngot: %s\n", exp2, rSring2)
+	}
+
+	rSring3 := refListToString(rList3, true)
+	if rSring3 != exp3 {
+		t.Fail()
+		fmt.Printf("TestRefToString / exp3 - rString3 mismatch:\nexp: %s\ngot: %s\n", exp3, rSring3)
+	}
+
+	rSring4 := refListToString(rList4, false)
+	if rSring4 != exp4 {
+		t.Fail()
+		fmt.Printf("TestRefToString / exp4 - rString4 mismatch:\nexp: %s\ngot: %s\n", exp4, rSring4)
+	}
+}
